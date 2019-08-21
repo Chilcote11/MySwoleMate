453b4da4d6b696d53c4a3007415f4300b357dceb
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-08-21 17:00:47 -0400

    create ScheduleBLL class
    used to pass data from DAL to Presentation Layer

---
+++ MySwoleMate.BLL/ScheduleBLL.cs
@@ -0,0 +1,47 @@
+﻿using MySwoleMate.DAL;
+using MySwoleMate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySwoleMate.BLL
+{
+    public class ScheduleBLL
+    {
+        private ScheduleDAL data;
+
+        public ScheduleBLL()
+        {
+            data = new ScheduleDAL();
+        }
+
+        public List<Schedule> getAllSchedules()
+        {
+            return data.GetAllSchedules();
+        }
+
+        public Schedule GetScheduleById(int id)
+        {
+            return data.GetScheduleById(id);
+        }
+
+        public void AddSchedule(Schedule schedule)
+        {
+            data.AddSchedule(schedule);
+        }
+
+        public void EditSchedule(Schedule schedule)
+        {
+            data.EditSchedule(schedule);
+        }
+
+        public void DeleteSchedule(Schedule schedule)
+        {
+            data.DeleteSchedule(schedule);
+        }
+
+        // TODO: create any display formatting methods?
+    }
+}
